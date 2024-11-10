c98c22df2a700f56c1933e9d60982c05333af4f4
Utkarsh Kumar <utk994@gmail.com>
2024-11-10 17:47:59 +0530

    Refactor project

---
+++ react-native/babel.config.js
@@ -0,0 +1,10 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    plugins: [
+      ["react-native-worklets-core/plugin"],
+      ["react-native-reanimated/plugin"],
+    ],
+    presets: ["babel-preset-expo"],
+  };
+};
